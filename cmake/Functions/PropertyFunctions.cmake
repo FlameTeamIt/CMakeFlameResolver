6d297e5f7eaa7c7069e2ad98696f938bcfdc3252
Anton Kashcheev <antonkachsheev@gmail.com>
2020-12-15 07:12:38 +0300

    Remove 'target_property_set()'

--- cmake/Functions/PropertyFunctions.cmake
+++ cmake/Functions/PropertyFunctions.cmake
@@ -1,16 +1,6 @@
 #
 #
 #
-function(target_property_set TARGET_NAME PROPERTY_NAME PROPERTY_VALUE)
-	set_target_properties(
-		${TARGET_NAME}
-		PROPERTIES ${PROPERTY_NAME} "${PROPERTY_VALUE}"
-	)
-endfunction(target_property_set)
-
-#
-#
-#
 function(target_property_get TARGET_NAME PROPERTY_NAME PROPERTY_VALUE)
 	get_target_property("${PROPERTY_VALUE}"
 		${TARGET_NAME}
