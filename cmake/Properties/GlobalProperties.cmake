766afeefd62d5ea8b695fb6f36635353d8293f3f
Anton Kashcheev <antonkachsheev@gmail.com>
2020-12-15 06:24:59 +0300

    cmake/Properties/GlobalProperties.cmake: remove quotes

--- cmake/Properties/GlobalProperties.cmake
+++ cmake/Properties/GlobalProperties.cmake
@@ -1,37 +1,37 @@
 define_property(GLOBAL
-	PROPERTY   "FLAME_HEADER_TARGETS"
+	PROPERTY   FLAME_HEADER_TARGETS
 	BRIEF_DOCS "List of header targets"
 	FULL_DOCS  "List of header targets"
 )
 
 define_property(GLOBAL
-	PROPERTY   "FLAME_OBJECT_TARGETS"
+	PROPERTY   FLAME_OBJECT_TARGETS
 	BRIEF_DOCS "List of object targets"
 	FULL_DOCS  "List of object targets"
 )
 
 define_property(GLOBAL
-	PROPERTY   "FLAME_STATIC_TARGETS"
+	PROPERTY   FLAME_STATIC_TARGETS
 	BRIEF_DOCS "List of static library targets"
 	FULL_DOCS  "List of static library targets"
 )
 
 define_property(GLOBAL
-	PROPERTY   "FLAME_SHARED_TARGETS"
+	PROPERTY   FLAME_SHARED_TARGETS
 	BRIEF_DOCS "List of shared library targets"
 	FULL_DOCS  "List of shared library targets"
 )
 
 define_property(GLOBAL
-	PROPERTY   "FLAME_BINARY_TARGETS"
+	PROPERTY   FLAME_BINARY_TARGETS
 	BRIEF_DOCS "List of binary targets"
 	FULL_DOCS  "List of binary targets"
 )
 
 set(FLAME_GLOBAL_PROPERTY_LIST
-	"FLAME_HEADER_TARGETS"
-	"FLAME_OBJECT_TARGETS"
-	"FLAME_STATIC_TARGETS"
-	"FLAME_SHARED_TARGETS"
-	"FLAME_BINARY_TARGETS"
+	FLAME_HEADER_TARGETS
+	FLAME_OBJECT_TARGETS
+	FLAME_STATIC_TARGETS
+	FLAME_SHARED_TARGETS
+	FLAME_BINARY_TARGETS
 )
