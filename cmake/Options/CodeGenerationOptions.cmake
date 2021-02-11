79953688d10088998d8a60556855422e0a0a7124
Anton Kashcheev <antonkachsheev@gmail.com>
2021-02-11 04:33:55 +0300

    Move options to other files; sort by categogies

---
+++ cmake/Options/CodeGenerationOptions.cmake
@@ -0,0 +1,19 @@
+# code generation and install options
+
+option(FLAME_ONLY_POSITION_INDEPENDENT_OBJECTS
+	"Static and shared libraries are using one object library"
+	ON
+)
+option(FLAME_MAKE_STATIC
+	"Creating static libraries"
+	ON
+)
+option(FLAME_MAKE_SHARED
+	"Creating shared libraries"
+	ON
+)
+
+option(FLAME_MAKE_STANDALONE
+	"Make all modules as standalone. Not support"
+	OFF
+)
